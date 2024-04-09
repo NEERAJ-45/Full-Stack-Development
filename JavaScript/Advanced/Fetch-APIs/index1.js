4e970f5a906640ef4c5c2101364000f794383d40
neeraj <neerajsurnis143r@gmail.com>
2024-04-09 12:01:07 +0530

    s

--- JavaScript/Advanced/Fetch-APIs/index1.js
+++ JavaScript/Advanced/Fetch-APIs/index1.js
@@ -1,7 +1,7 @@
 console.clear();
-
+/** 
 fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-  .then((response) => {
+.then((response) => {
     if (!response.ok) {
       throw new Error("Could not Fetch! Error");
     }
@@ -9,3 +9,4 @@ fetch("https://pokeapi.co/api/v2/pokemon/ditto")
   })
   .then((data) => console.log(data.id))
   .catch((error) => console.error(error));
+*/
