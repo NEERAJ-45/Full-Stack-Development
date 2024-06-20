b982838ca9ed7372002ab9799401b1bce53d2c15
neeraj <neerajsurnis143r@gmail.com>
2024-06-20 10:06:41 +0530

    d

---
+++ PROJECTS/Mini-Projects/Theme-Changer/script.js
@@ -0,0 +1,16 @@
+const greyBox = document.getElementById("greyBox");
+greyBox.addEventListener("click", () => {
+  document.body.style.backgroundColor = "grey";
+});
+const whiteBox = document.getElementById("whiteBox");
+whiteBox.addEventListener("click", () => {
+  document.body.style.backgroundColor = "white";
+});
+const blueBox = document.getElementById("blueBox");
+blueBox.addEventListener("click", () => {
+  document.body.style.backgroundColor = "blue";
+});
+const yellowBox = document.getElementById("yellowBox");
+yellowBox.addEventListener("click", () => {
+  document.body.style.backgroundColor = "yellow";
+});
