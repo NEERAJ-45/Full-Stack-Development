3c22f38511d7ba79975eb4fc991528194474885a
neeraj <neerajsurnis143r@gmail.com>
2024-08-07 00:12:38 +0530

    ssd

--- Backend/Backend-SCS/MongoDB/CRUD_EJS/server.js
+++ Backend/Backend-SCS/MongoDB/CRUD_EJS/server.js
@@ -13,12 +13,26 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 app.get("/delete/:id", async (req, res) => {
-    let users = await userModel.findOneAndDelete({_id:req.params.id});
-    res.redirect('/read')
+  let users = await userModel.findOneAndDelete({ _id: req.params.id });
+  res.redirect("/read");
+});
+app.post("/update/:id", async (req, res) => {
+  let { name, email, image } = req.body;
+
+  let user = await userModel.findOneAndUpdate(
+    { _id: req.params.id },
+    { image, email, name },
+    { new: true }
+  );
+  res.redirect("/read");
+});
+app.get("/edit/:id", async (req, res) => {
+  let user = await userModel.findOne({ _id: req.params.id });
+  res.render("edit", { user });
 });
 app.get("/read", async (req, res) => {
   let users = await userModel.find();
-  res.render("read",{users});
+  res.render("read", { users });
 });
 app.post("/create", async (req, res) => {
   let { name, email, image } = req.body;
@@ -28,7 +42,7 @@ app.post("/create", async (req, res) => {
     image,
   });
 
-  res.redirect('/read');
+  res.redirect("/read");
 });
 
 app.listen(port, () => {
