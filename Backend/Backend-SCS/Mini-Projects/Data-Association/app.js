caed97bc3e1c9ee80adc91f2d14e63ac90c4d5e2
neeraj <neerajsurnis143r@gmail.com>
2024-08-18 15:57:21 +0530

    MiniProject

--- Backend/Backend-SCS/Mini-Projects/Data-Association/app.js
+++ Backend/Backend-SCS/Mini-Projects/Data-Association/app.js
@@ -1,29 +1,94 @@
 const express = require("express");
 const port = 3000;
 const app = express();
+const path = require("path");
+
 const userModel = require("./models/user");
 const postModel = require("./models/post");
 const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+const user = require("./models/user");
+const multerConfig = require("./config/multerconfig");
+const exp = require("constants");
+const upload = require("./config/multerconfig");
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("index");
 });
+app.get("/profile/upload", (req, res) => {
+  res.render("profileupload");
+});
+app.post("/upload", isLoggedIn, upload.single("image"), async (req, res) => {
+  let user = await userModel.findOne({ email: req.user.email });
+  user.profilepic = req.file.filename;
+  await user.save();
+
+  res.redirect("/profile");
+});
+
 app.get("/login", (req, res) => {
   res.render("login");
 });
-app.get("/profile", isLoggedIn, (req, res) => {
-  res.render("login");
+app.get("/profile", isLoggedIn, async (req, res) => {
+  let user = await userModel
+    .findOne({ email: req.user.email })
+    .populate("posts");
+  res.render("profile", { user });
 });
+app.get("/like/:id", isLoggedIn, async (req, res) => {
+  let post = await postModel.findOne({ _id: req.params.id }).populate("user");
+
+  if (post.likes.indexOf(req.user.userid) === -1) {
+    post.likes.push(req.user.userid);
+  } else {
+    post.likes.splice(post.likes.indexOf(req.user.userid), 1);
+  }
+
+  await post.save();
+  res.redirect("/profile");
+});
+app.get("/edit/:id", isLoggedIn, async (req, res) => {
+  let post = await postModel.findOne({ _id: req.params.id }).populate("user");
+
+  res.render("edit", { post });
+});
+app.post("/update/:id", isLoggedIn, async (req, res) => {
+  let post = await postModel.findOneAndUpdate(
+    { _id: req.params.id },
+
+    { content: req.body.content }
+  );
+
+  res.redirect("/profile");
+});
+
+app.post("/post", isLoggedIn, async (req, res) => {
+  let user = await userModel.findOne({ email: req.user.email });
+  let { content } = req.body;
+
+  let post = await postModel.create({
+    user: user._id,
+
+    content,
+  });
+
+  user.posts.push(post._id);
+  await user.save();
+  res.redirect("/profile");
+});
+
 app.get("/logout", (req, res) => {
   res.cookie("token", "");
   res.redirect("/login");
 });
+
 app.post("/register", async (req, res) => {
   let { email, username, password, name, age } = req.body;
   let user = await userModel.findOne({ email });
@@ -48,20 +113,19 @@ app.post("/login", async (req, res) => {
   let { email, password } = req.body;
   let user = await userModel.findOne({ email });
   if (!user) res.status(500).send("Something went wrong!");
- 
+
   bcrypt.compare(password, user.password, (err, result) => {
     if (result) {
       let token = jwt.sign({ email: email, userid: user._id }, "MONDAY");
       res.cookie("token", token);
-      res.status(200).send("You can login");
+      res.status(200).redirect("/profile");
     } else res.redirect("/login");
   });
 });
 
 function isLoggedIn(req, res, next) {
   if (req.cookies.token === "") {
-
-   return res.send("U must be logged in");
+    return res.redirect("/login");
   } else {
     let data = jwt.verify(req.cookies.token, "MONDAY");
     req.user = data;
--- Backend/Backend-SCS/Mini-Projects/Data-Association/models/post.js
+++ Backend/Backend-SCS/Mini-Projects/Data-Association/models/post.js
@@ -1,19 +1,26 @@
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/Mini-Project1");
+
 const postSchema = mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "post",
+    ref: "User",
+    required: true,
   },
   date: {
     type: Date,
     default: Date.now,
   },
-  content: String,
-  likes: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "user",
+  content: {
+    type: String,
+    required: true,
   },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    }
+  ], // An array to store multiple user references who liked the post
 });
 
 module.exports = mongoose.model("post", postSchema);
