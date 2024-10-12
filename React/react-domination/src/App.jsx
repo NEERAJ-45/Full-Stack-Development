c159102e82eb22a2b9a778ea18a8c21cc18d13c7
Neeraj <neerajsurnis143r@gmail.com>
2024-10-12 18:51:19 +0530

    Praticed useState

--- React/react-domination/src/App.jsx
+++ React/react-domination/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 const Fun = () => {
   const [usersname, setusername] = useState("Neeraj");
+  const [usersno, setuserno] = useState(0);
   const changeName = () => {
     setusername('Moin');
   };
@@ -9,8 +10,11 @@ const Fun = () => {
   return (
     <div>
       <h1>{usersname} will be GOAT🐐</h1>
+      <br></br>  
+      <h3>Current value:{usersno} </h3>
       <button onClick={changeName}>Change User</button>
-      <h1>{usersname} will be Mine</h1>
+      <button onClick={()=>setuserno(usersno+10)}>Increment</button>
+      <button onClick={()=>setuserno(usersno-10)}>Decrement</button>
     </div>
   );
 };
