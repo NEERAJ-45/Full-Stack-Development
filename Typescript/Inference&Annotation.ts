98a1d4d9e28405288fdc40c8df5cf71ca9e653eb
Neeraj! <neerajsurnis143r@gmail.com>
2024-12-06 18:23:17 +0530

    s

--- Typescript/Inference&Annotation.ts
+++ Typescript/Inference&Annotation.ts
@@ -2,4 +2,4 @@
 let message = "Hello, world!"; // TypeScript infers that 'message' is of type 'string'
 
 //# Type annotations are explicit declarations of the types of variables, function parameters, return values, etc. They are useful when you want to be explicit about the types or when TypeScript cannot infer the type correctly.
-let name2: string = "John Doe"; // Explicitly declare 'name' as a 'string
+let name2: string = "John Doe"; // Explicitly declare 'name' as a 'string;
